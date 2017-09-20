1eba21391319bd8ac4900d3f0c7612834f3a4773
Jamaal Crichfield <jcrichfield2@gmail.com>
2017-09-19 21:07:57 -0400

    Re-arranged the conditional statements to check for all angles being zero, also took out the defintion of NULL that was set to zero and just initialized the angle vars to be zero

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
-#define NULL 0
 #include <stdio.h>
 
 int main() {
-  int  angleA;
-  int  angleB;
-  int  angleC;
+  int  angleA = 0;
+  int  angleB = 0;
+  int  angleC = 0;
   int missingAng;
 
   printf("INSTRUCTIONS: Please input values for three angles. \nIf any given angle is undefined please enter a zero. Program will only except comma seperate values: ");
@@ -13,17 +12,20 @@ int main() {
   if ( angleA > 0 && angleB > 0 && angleC > 0 ) {
     printf("The angles of your triangle are: %d, %d, and %d \n", angleA, angleB, angleC);
   }
-  else if ( angleA == NULL ) {
+  else if ( angleA == 0 && angleB == 0 && angleC == 0 ) {
+    printf("angle(s) A, B, and C are all undefined \n");
+  }
+  else if ( angleA == 0 ) {
     printf("angle A has no value \n");
     missingAng = 180 - (int)(angleB + angleC);
     printf("Missing angle A is: %d degrees \n", missingAng);
   }
-  else if ( angleB == NULL ) {
+  else if ( angleB == 0 ) {
     printf("angle B has no value \n");
     missingAng = 180 - (angleC + angleA);
     printf("Missing angle B is: %d degrees \n", missingAng);
   }
-  else if ( angleC == NULL ) {
+  else if ( angleC == 0 ) {
     printf("angle C hase no value \n");
     missingAng = 180 - (angleA + angleB);
     printf("Missing angle C is: %d \n", missingAng);
